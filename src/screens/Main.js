f259ded08e55ac3935fcc91c6400a94ab9138b7b
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-09 21:58:05 +0700

    add conditional rendering for is login

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'native-base';
 
 // import screens
 import Root from './Root';
@@ -12,37 +14,82 @@ import Detail from './Detail';
 
 // import navigator
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const MainStack = createStackNavigator();
+
+function PrivateStackScreen() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#2395FF',
+        inactiveTintColor: 'gray',
+      }}>
+      <Tab.Screen
+        name="Private_Home"
+        component={Home}
+        options={{
+          tabBarIcon: (focused, color, size) => (
+            <Icon
+              type="MaterialIcons"
+              name="home"
+              size={size}
+              color={color}
+            />
+          ),
+          title: 'Home',
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function Main() {
+  const isLogin = true;
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Root"
-          component={Root}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Public_Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Detail"
-          component={Detail}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      {!isLogin ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Root"
+            component={Root}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Public_Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Detail"
+            component={Detail}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      ) : (
+          <MainStack.Navigator>
+            <MainStack.Screen
+              name="Main"
+              component={PrivateStackScreen}
+              options={{ headerShown: false }}
+            />
+            <MainStack.Screen
+              name="Detail"
+              component={Detail}
+              options={{ headerShown: false }}
+            />
+          </MainStack.Navigator>
+        )}
     </NavigationContainer>
   );
 }
