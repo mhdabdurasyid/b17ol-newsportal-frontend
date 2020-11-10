a682d5e7c7c6ef4d9ac071faf2881618a97cf306
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-11 04:38:55 +0700

    add http helper

---
+++ src/helpers/http.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import axios from 'axios';
+import { API_URL } from '@env';
+
+const http = (token = false) => {
+  return axios.create({
+    baseURL: API_URL,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+  });
+};
+
+export default http;
