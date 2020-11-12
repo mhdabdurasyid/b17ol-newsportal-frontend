08a9145b3e21cca4c2e368b60c4f943a29b0c2ae
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-12 22:15:48 +0700

    using react-native-splash-screen to fixed splashscreen transition

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
@@ -9,6 +10,10 @@ import {store, persistor} from './src/redux/store';
 import Main from './src/screens/Main';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
