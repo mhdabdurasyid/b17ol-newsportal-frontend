20c8a917e8797c56ad1e587e21738a094ae8c3ca
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-12-10 15:44:12 +0700

    add form validation on edit profile screen

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -13,6 +13,8 @@ import {View, StyleSheet, Alert, TouchableOpacity} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {API_URL} from '@env';
 import ImagePicker from 'react-native-image-picker';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 // import actions
 import profileAction from '../redux/actions/profile';
@@ -25,24 +27,29 @@ export default function EditProfile({navigation}) {
   const auth = useSelector((state) => state.auth);
   const profile = useSelector((state) => state.profile);
 
-  const [name, setName] = useState(profile.profileData.name);
-  const [email, setEmail] = useState(profile.profileData.email);
+  const schema = Yup.object().shape({
+    name: Yup.string().required('Fullname field is required'),
+    email: Yup.string()
+      .email('Please enter correct email')
+      .required('Email field is required'),
+  });
+
   const [photo, setPhoto] = useState(profile.profileData.photo);
   const [imgData, setImgData] = useState(null);
 
   useEffect(() => {
     if (profile.isEdit) {
-      Alert.alert('Edit profile succesful..');
+      Alert.alert('Success!', 'Edit profile succesfully.');
       dispatch(profileAction.resetEdit());
       dispatch(profileAction.getProfile(auth.token));
       navigation.navigate('Profile');
     }
   });
 
-  function doEditProfile() {
+  function doEditProfile(values) {
     const form = new FormData();
-    form.append('email', email);
-    form.append('name', name);
+    form.append('email', values.email);
+    form.append('name', values.name);
     if (imgData) {
       form.append('image', imgData);
     }
@@ -81,43 +88,72 @@ export default function EditProfile({navigation}) {
 
   return (
     <Container>
-      <Content style={styles.padding}>
-        <Text style={[styles.bold, styles.header]}>Edit your profile</Text>
-        <View style={styles.avatar}>
-          <TouchableOpacity onPress={selectImage}>
-            <Thumbnail
-              source={
-                photo !== null
-                  ? {
-                      uri: photo.includes('upload')
-                        ? `${API_URL}${photo}`
-                        : photo,
-                    }
-                  : User
-              }
-            />
-          </TouchableOpacity>
-        </View>
-        <Item style={styles.marginBottom}>
-          <Icon type="MaterialIcons" name="person" style={styles.blue} />
-          <Input
-            value={name}
-            style={styles.fontSize_14}
-            onChangeText={(text) => setName(text)}
-          />
-        </Item>
-        <Item style={styles.marginBottom}>
-          <Icon type="MaterialIcons" name="email" style={styles.blue} />
-          <Input
-            value={email}
-            style={styles.fontSize_14}
-            onChangeText={(text) => setEmail(text)}
-          />
-        </Item>
-        <Button rounded block style={styles.btn} onPress={doEditProfile}>
-          <Text style={styles.white}>{'  Save  '}</Text>
-        </Button>
-      </Content>
+      <Formik
+        initialValues={{
+          name: profile.profileData.name,
+          email: profile.profileData.email,
+        }}
+        validationSchema={schema}
+        onSubmit={(values) => doEditProfile(values)}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          touched,
+          errors,
+        }) => (
+          <Content style={styles.padding}>
+            <Text style={[styles.bold, styles.header]}>Edit your profile</Text>
+            <View style={styles.avatar}>
+              <TouchableOpacity onPress={selectImage}>
+                <Thumbnail
+                  source={
+                    photo !== null
+                      ? {
+                          uri: photo.includes('upload')
+                            ? `${API_URL}${photo}`
+                            : photo,
+                        }
+                      : User
+                  }
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.marginBottom}>
+              <Item>
+                <Icon type="MaterialIcons" name="person" style={styles.blue} />
+                <Input
+                  onChangeText={handleChange('name')}
+                  onBlur={handleBlur('name')}
+                  value={values.name}
+                  style={styles.fontSize_14}
+                />
+              </Item>
+              {touched.name && errors.name && (
+                <Text style={styles.error}>{errors.name}</Text>
+              )}
+            </View>
+            <View style={styles.marginBottom}>
+              <Item>
+                <Icon type="MaterialIcons" name="email" style={styles.blue} />
+                <Input
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  style={styles.fontSize_14}
+                />
+              </Item>
+              {touched.email && errors.email && (
+                <Text style={styles.error}>{errors.email}</Text>
+              )}
+            </View>
+            <Button rounded block style={styles.btn} onPress={handleSubmit}>
+              <Text style={styles.white}>{'  Save  '}</Text>
+            </Button>
+          </Content>
+        )}
+      </Formik>
     </Container>
   );
 }
@@ -154,5 +190,9 @@ const styles = StyleSheet.create({
   btn: {
     backgroundColor: '#2395FF',
     marginTop: 32,
+  },
+  error: {
+    fontSize: 12,
+    color: 'red',
   },
 });
